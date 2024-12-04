5680c0e7e267ad0ed9556bf09332c06e21eab1e9
MSouch <msouch3@gmail.com>
2024-12-04 10:40:44 -0700

    Update comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import '../styles/loader.css'; // Import the loader CSS here
+import '../styles/loader.css'; 
 import Header from '@/components/Header';
 
 const geistSans = localFont({
